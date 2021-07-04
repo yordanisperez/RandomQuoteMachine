91fc5dfbc8f39a3b6f85b6a83223f199fe697eee
yordanis <yordanis@gmail.com>
2021-07-04 11:27:28 -0400

    Cambio en el estilo de la targeta

--- src/RandomQuoteMachine.js
+++ src/RandomQuoteMachine.js
@@ -1,6 +1,6 @@
 import './RandomQuoteMachine.css';
 import Card from './component/ui/Card'
-import React, { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 const urlQuotes ="https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json";
 const urlTweet="https://twitter.com/intent/tweet?hashtags=quotes&related=freecodecamp&text="
 
